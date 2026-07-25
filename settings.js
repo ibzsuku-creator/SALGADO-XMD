868444b0fa9a80f57c13c557dbc8a60b41fc474e
ibzsuku-creator <ibzsuku@gmail.com>
2026-07-25 23:36:50 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    botName: 'SALGA-XMD',
+    botOwner: 'IBRAHIMA SORY SACKO',
+    ownerNumber: '224621963059',
+    version: '1.0.0',
+    commandMode: 'Public', // Public ou Private (modifiable via la commande "mode")
+    menuImage: 'https://i.ibb.co/mVwPDPZG/file-000000002d5081f4a61a8a350d76d247.png',
+    system: 'CENTRAL-HEX'
+};
+
